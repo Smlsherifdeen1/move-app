0c353369bd915fa3f207ccd407b90b3e76ec91b1
smlsherifdeen1 <smlsherifdeen@gmail.com>
2026-09-14 10:14:01 -0700

    Fix service worker cache updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='move-v1'; const ASSETS=['/','/index.html','/manifest.json','/icon-192.svg','/icon-512.svg'];
+const CACHE='move-v2'; const ASSETS=['/','/index.html','/manifest.json','/icon-192.svg','/icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('/'))))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(c=>c||caches.match('/'))))});
